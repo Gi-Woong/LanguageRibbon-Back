cf18dabb06d8304b6205e9833a52194a2578ed3e
KimSH39 <sooooooohyeon5@naver.com>
2023-11-06 11:14:12 +0900

    feat: 회원가입 API 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    sex = models.IntegerField()
+    age = models.IntegerField()
+    job = models.IntegerField()
+    englishLevel = models.IntegerField()
+
+    class Meta:
+        app_label = 'accounts'
