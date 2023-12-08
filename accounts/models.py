5c9c9fc58fbc37ed7832d6700b7f1bd5e70b5c3d
KimSH39 <sooooooohyeon5@naver.com>
2023-12-08 18:24:21 +0900

    fix: model에 초기 목소리 정보 추가

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,8 @@ class UserProfile(models.Model):
     age = models.IntegerField()
     job = models.CharField(max_length=200)
     englishLevel = models.CharField(max_length=10)
+    voice_info_en = models.CharField(max_length=200, null=True, blank=True)
+    voice_info_kr = models.CharField(max_length=200, null=True, blank=True)
 
     class Meta:
         db_table = 'accounts_userprofile'
