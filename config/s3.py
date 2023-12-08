4f541fca824d1f220844e237f79f027255a7027b
Gi-Woong <rldndco@mju.ac.kr>
2023-12-08 19:10:19 +0900

    feature: AWS S3 설정 추가

---
+++ config/s3.py
@@ -0,0 +1,14 @@
+import boto3
+from botocore.client import Config
+from config.settings import ENV
+
+aws_access_key_id = ENV("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = ENV("AWS_SECRET_ACCESS_KEY")
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    config=Config(signature_version='s3v4'),
+    region_name="ap-northeast-2"
+)
